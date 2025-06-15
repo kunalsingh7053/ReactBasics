2e0367c97d088a67f14c4db885f6aa84e87e6387
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-06-15 18:32:58 +0530

    crud project responsive and tostify&fromhook

--- crudproject/src/App.jsx
+++ crudproject/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { nanoid } from 'nanoid';
 import Create from "./components/Create";
 import Read from "./components/Read";
-
+import SplashCursor from "../Reactbit/SplashCursor/SplashCursor";
+import TextPressure from "../Textanimation/TextPressure/TextPressure";
 const App = () => {
 
   const [todos, settodos] = useState([
@@ -14,10 +15,15 @@ const App = () => {
 
  
   return (
-    <div>
-   <Create todos={todos} settodos={settodos}/>
-      <hr />
-    <Read todos={todos} settodos={settodos}/>
+    <div className="text-white flex flex-col w-screen h-screen bg-black p-3">
+    <SplashCursor />
+     <div className="h-[10%] sm:mb-[20px] md:mb-[40px] lg:mb-[60px] xl:[80px] 2xl:mb-[150px]">
+    <TextPressure />
+  </div>
+    <div className="flex  h-[90%] ">
+        <Create todos={todos} settodos={settodos} />
+        <Read todos={todos} settodos={settodos} />
+      </div>
     </div>
   )
 }
--- crudproject/src/components/Create.jsx
+++ crudproject/src/components/Create.jsx
@@ -1,42 +1,46 @@
 import { useState } from "react";
 import { nanoid } from 'nanoid';
-
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 const Create = (props) => {
     const todos = props.todos;
     const settodos = props.settodos;
-      const [title, settitle] = useState("")
+  
 
-      const submithandler =(e)=>{
-e.preventDefault();
+      const {
+        register,
+        handleSubmit,
+        reset,
+        formState:{errors}
+      } = useForm();
+      const submithandler =(data)=>{
+    data.id = nanoid();
+    data.iscompleted = false;
+  settodos([...todos, data]);
+  toast.success("Todo Created!✅");
+  reset();
 
-  if (title.trim() === "") {
-    alert("Please enter a todo before submitting.");
-    return;
-  }
-const newtodos ={
-  id:nanoid(),
-  title,
-  iscompleted:false
-}
-settodos([...todos,newtodos])
-console.log(todos);
-settitle("");
+
   }
  
   return (
-    <>
-         <h1>Create Your Todo</h1>
-      <form onSubmit={submithandler}>
+    <div className="border w-[60%] p-2 text-center">
+         <h1 className="text-2xl lg:text-3xl font-thin">Set <span className="text-red-400">Reminders</span>  for tasks</h1>
+      <form onSubmit={handleSubmit(submithandler)}>
         
         <input
-        value={title}
-        onChange={(e)=>settitle(e.target.value)}
+        {...register("title",{
+          required:"title can not be empty"
+        })}
+        placeholder="Title"
+        className="p-1 border-b w-full text-1xl lg:text-2xl font-thin outline-0"
         type="text" />
-        <button type="submit">Creat</button>
-        <button type="button" onClick={() => settodos([])}>Clear All</button>
+        {errors && errors.title && errors.title.message && <p className="text-[red]">{ errors.title.message}</p>}
+        <button className="mt-10  text-[15px] lg:text-[20px] lg:mr-[10px] px-8 py-1 border rounded" type="submit">Creat</button>
+        <button  className="mt-10 text-[15px] lg:text-[20px]  px-8 py-1 border rounded" type="button" onClick={() => settodos([])}>Clear All</button>
 
       </form>
-    </>
+    </div>
   )
 }
 
